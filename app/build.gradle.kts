adee5a65fdb22a6bf238e90c323d9d199fc4a680
Peransss <frans04102006@gmail.com>
2026-06-06 17:31:14 +0700

    chore(redesign): remove old Fragment/View/XML code, drop viewBinding
    - Delete legacy auth/, staff/, patient/, adapter/ packages
    - Delete res/layout, res/navigation, res/menu
    - Remove viewBinding = true from app/build.gradle.kts
    - App is now 100% Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
     }
     buildFeatures {
         compose = true
-        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
